fca103a480bf9d7c3fb58f49e40c930a7720b9ee
Richard George <richard.jhn.george.3@gmail.com>
2023-05-09 16:45:44 +0100

    fixed the tests

--- jest.config.js
+++ jest.config.js
@@ -28,6 +28,7 @@ const config = {
   moduleNameMapper: {
     "\\.(css|less|sass|scss)$": "<rootDir>/src/__mocks__/styleMock.js",
     "\\.(gif|ttf|eot|svg)$": "<rootDir>/src/__mocks__/fileMock.js",
+    '\\.(jpg|jpeg|png|gif)$': 'identity-obj-proxy',
   },
 };
 
